b94c8db9147e96915d860c80dc30e5eee4d683cb
Esteban Sevilla <esteban@perspective.com.mx>
2022-03-27 15:58:15 -0600

    77. Centralizar los widgets personalizados

--- S6 Componentes de flutter/fl_components/lib/screens/card_screen.dart
+++ S6 Componentes de flutter/fl_components/lib/screens/card_screen.dart
@@ -6,9 +6,21 @@ class CardScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal:20,vertical: 10),
+        children: const [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.book_outlined),
+              title: Text('Soy un titulo'),
+              subtitle: Text('Exercitation incididunt aliqua cillum sit nostrud Lorem mollit.Non consequat et irure culpa proident.'),
+            ),
+          )
+        ],
       ),
     );
   }

6ef2a5c37e9107419816086de502631905c7cc25
Esteban Sevilla <esteban@perspective.com.mx>
2022-03-30 08:54:51 -0600

    88. TextFormFields - Personalización

--- S6 Componentes de flutter/fl_components/lib/screens/inputs_screen.dart
+++ S6 Componentes de flutter/fl_components/lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -16,10 +17,42 @@ class InputsScreen extends StatelessWidget {
            child: Column(
             children: [
               TextFormField(
-                autofocus: true,
-                initialValue: 'Pedro ',
+                // autofocus: true,
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (text){print('Texto $text');},
+                validator: (text){
+                  if(text==null){
+                    return 'Campo vacío';
+                  }
+                  if(text.isEmpty){
+                    return 'Campo vacío';
+                  }
+                  return null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction, // Cuando el usuario empieze a escribir
+                decoration: const InputDecoration(
+                  hintText: 'Escribe tu nombre',
+                  labelText: 'Nombre Completo',
+                  helperText: 'Sólo letras',
+                  counterText: 'min: 3',
+                  icon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.person_add_alt_outlined),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppTheme.primaryLigthColor
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppTheme.primaryLigthColor
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(20),
+                      topLeft: Radius.circular(20)
+                    )
+                  )
+                ),
               )
         ]),
          ),

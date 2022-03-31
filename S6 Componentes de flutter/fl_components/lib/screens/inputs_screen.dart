19c19eed17841c473c04d6d30a082f4a08dea091
Esteban Sevilla <esteban@perspective.com.mx>
2022-03-30 21:34:13 -0600

    92. Valores del formulario

--- S6 Componentes de flutter/fl_components/lib/screens/inputs_screen.dart
+++ S6 Componentes de flutter/fl_components/lib/screens/inputs_screen.dart
@@ -10,6 +10,13 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    final Map<String,String> mapa = {
+      'nombre':'Pedro Sevilla'
+    };
+
+    final GlobalKey<FormState> globalKey = GlobalKey<FormState>();
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Inputs Screen')),
@@ -17,48 +24,66 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
          child: Padding(
            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-           child: Column(
-            children: const [
-              CustomTextField1(
-                hintText: 'Escribe tu(s) nombre(s)', 
-                labelText: 'Nombre(s)', 
-                helperText: 'Sólo ingresa tu(s) nombre(s)', 
-                icon: Icon(Icons.person), 
-                suffixIcon: Icon(Icons.person_pin_outlined)),
-
-                SizedBox(height: 30,),
-
-              CustomTextField1(
-                hintText: 'Escribe tu(s) apellido(s)', 
-                labelText: 'Apellido(s)', 
-                helperText: 'Sólo ingresa letras', 
-                icon: Icon(Icons.queue_play_next_outlined), 
-                suffixIcon: Icon(Icons.plagiarism_rounded)),
-
-                SizedBox(height: 30,),
-
-
-              CustomTextField1(
-                hintText: 'Escribe tu correo', 
-                labelText: 'Correo', 
-                helperText: 'Ingresa un correo real', 
-                icon: Icon(Icons.church),
-                textInputType: TextInputType.emailAddress,
-                suffixIcon: Icon(Icons.email)),
-
-                SizedBox(height: 30,),
-
-
-              CustomTextField1(
-                hintText: 'Escribe tu password', 
-                labelText: 'Password', 
-                // helperText: 'Ingresa ', 
-                icon: Icon(Icons.password),
-                obscureText: true,
-                // textInputType: TextInputType.emailAddress,
-                suffixIcon: Icon(Icons.password)),
-
-        ]),
+           child: Form(
+             key: globalKey,
+             child: Column(
+              children: [
+                const CustomTextField1(
+                  hintText: 'Escribe tu(s) nombre(s)', 
+                  labelText: 'Nombre(s)', 
+                  helperText: 'Sólo ingresa tu(s) nombre(s)', 
+                  icon: Icon(Icons.person), 
+                  suffixIcon: Icon(Icons.person_pin_outlined)),
+           
+                  const SizedBox(height: 30,),
+           
+                const CustomTextField1(
+                  hintText: 'Escribe tu(s) apellido(s)', 
+                  labelText: 'Apellido(s)', 
+                  helperText: 'Sólo ingresa letras', 
+                  icon: Icon(Icons.queue_play_next_outlined), 
+                  suffixIcon: Icon(Icons.plagiarism_rounded)),
+           
+                  const SizedBox(height: 30,),
+           
+           
+                const CustomTextField1(
+                  hintText: 'Escribe tu correo', 
+                  labelText: 'Correo', 
+                  helperText: 'Ingresa un correo real', 
+                  icon: Icon(Icons.church),
+                  textInputType: TextInputType.emailAddress,
+                  suffixIcon: Icon(Icons.email)),
+           
+                  const SizedBox(height: 30,),
+           
+           
+                const CustomTextField1(
+                  hintText: 'Escribe tu password', 
+                  labelText: 'Password', 
+                  // helperText: 'Ingresa ', 
+                  icon: Icon(Icons.password),
+                  obscureText: true,
+                  // textInputType: TextInputType.emailAddress,
+                  suffixIcon: Icon(Icons.password)),
+           
+                  const SizedBox(height: 30,),
+           
+                  ElevatedButton(onPressed: (){
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if(!globalKey.currentState!.validate()){
+                      return;
+                    }
+                  }, 
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: SizedBox(
+                      child: Center(child: Text('Enviar'),),
+                    ),
+                  ))
+           
+                   ]),
+           ),
          ),
       ),
     );

94c24a4524e81e92c2f4623bf58904e60267ac0e
Esteban Sevilla <esteban@perspective.com.mx>
2022-04-02 00:24:07 -0600

    98. InfiniteScroll con Futures

--- S6 Componentes de flutter/fl_components/lib/screens/listview_builder_screen.dart
+++ S6 Componentes de flutter/fl_components/lib/screens/listview_builder_screen.dart
@@ -1,7 +1,8 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class ListViewBuilderScreen extends StatefulWidget {
-   
+
   const ListViewBuilderScreen({Key? key}) : super(key: key);
 
   @override
@@ -11,6 +12,7 @@ class ListViewBuilderScreen extends StatefulWidget {
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
   final List<int> listIndexes = [1,2,3,4,5,6,7,8,9,10];
   final ScrollController _scrollController = ScrollController();
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -20,44 +22,89 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
   void _scrollListener(){
     if((_scrollController.position.pixels+500)>=_scrollController.position.maxScrollExtent){
-      print('entra');
-      addElementsToList();
+      fetchData();
+      // print('entra');
+      // addElementsToList();
     }
+  }
+
+  Future fetchData() async{
+    if(isLoading)return;
+
+    isLoading = true;
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    addElementsToList();
+
+    setState(() {});
+
+    isLoading = false;
   }
 
   void addElementsToList(){
     int last = listIndexes.length;
-    print(listIndexes);
 
     listIndexes.addAll(
       [1,2,3,4,5].map((e) => last+e)
     );
-    print(listIndexes);
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return  Scaffold(
       body: MediaQuery.removePadding(
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          
-          controller: _scrollController,
-          
-          itemCount: listIndexes.length,
-          //Widget Function(BuildContext, int)
-          itemBuilder: (context,index)=>FadeInImage(
-            width: double.infinity,
-            height: 200,
-            fit: BoxFit.cover,
-            placeholder: const AssetImage('assets/jar-loading.gif'), 
-            image: NetworkImage('https://picsum.photos/500/200?image=${listIndexes[index ]}')
-          )
+        child: Stack(
+          children: [
+            ListView.builder(
+
+              controller: _scrollController,
+
+              itemCount: listIndexes.length,
+              //Widget Function(BuildContext, int)
+              itemBuilder: (context,index)=>FadeInImage(
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+                placeholder: const AssetImage('assets/jar-loading.gif'),
+                image: NetworkImage('https://picsum.photos/500/200?image=${listIndexes[index ]}')
+              )
+            ),
+            Positioned(
+              bottom: 20,
+              left: size.width*.5-30,
+              child: const CustomProgressIndicator()
+            )
+          ],
         ),
       )
+    );
+  }
+}
+
+class CustomProgressIndicator extends StatelessWidget {
+  const CustomProgressIndicator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(.5),
+        shape: BoxShape.circle
+      ),
+      child: const CircularProgressIndicator(
+        color: AppTheme.primaryLigthColor,
+      ),
     );
   }
 }

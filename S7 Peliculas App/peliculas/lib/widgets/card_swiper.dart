a21053eb2f667a7c018a037fda296b653f0b1a38
Esteban Sevilla <esteban@perspective.com.mx>
2022-04-03 15:09:42 -0500

    109. Swiper - Carrusel de tarjetas

--- S7 Peliculas App/peliculas/lib/widgets/card_swiper.dart
+++ S7 Peliculas App/peliculas/lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -6,10 +7,37 @@ class CardSwiper extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Container(
-      height: 500,
+      height: size.height*.5,
       width: double.infinity,
       color: Colors.red,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemHeight: size.height*.4,
+        itemWidth: size.width*.5,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: (){
+              Navigator.pushNamed(context, '/details',arguments: 'vamos a por todo');
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),   
+              child: const FadeInImage(
+                image: NetworkImage('https://via.placeholder.com/288x188'),
+                placeholder: AssetImage('assets/no-image.jpg'),
+                fit: BoxFit.fill,
+              )
+              //  Image.network(
+              //   "https://via.placeholder.com/288x188",
+              //   fit: BoxFit.fill,
+              // ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
